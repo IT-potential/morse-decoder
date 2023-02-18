a8b87efc0ebfe38c17321cd93775399571905d81
Zlata Romanova <forofjob@gmail.com>
2023-02-19 02:13:14 +0700

    task finished

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = [];
+  let morse_code = '';
+for (let j = 0; j< expr.length; j+=10) {
+  let substr = expr.slice(j, j+10);
+  // console.log(substr);
+  arr.push('')
+
+for (let i = 0; i < substr.length; i+=2) {
+  if (substr[i] == 1) { 
+    let position = substr.slice(i, i+2);
+    if (position === '10') {
+        arr[arr.length-1] +=  position.replace("10", ".") 
+    }
+   else if (position === '11') {
+       arr[arr.length-1] += position.replace("11", "-")
+      //  console.log(arr)
+   }
+  }
+  
+  }
+} 
+for (let el of arr) {
+   morse_code += MORSE_TABLE[el] || ' '
+  //  console.log(morse_code);
 }
+return morse_code;
+}
+
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
 
 module.exports = {
     decode
